f2532bfe87a440aeeb254477c2167ce38a4b933b
ralfsiraks <hyperblazexd@gmail.com>
2024-03-15 00:11:37 +0200

    env fix

--- src/app/services/product.service.ts
+++ src/app/services/product.service.ts
@@ -1,8 +1,8 @@
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { environment } from '../../environments/environment';
 import { Pagination } from '../interfaces/pagination';
 import { Product } from '../interfaces/product';
-import { environment } from '../../environments/environment.development';
 
 @Injectable({
   providedIn: 'root',
