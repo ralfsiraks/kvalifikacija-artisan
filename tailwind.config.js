09df8f418cd4a75f93f8469035339f8ababf99ed
ralfsiraks <hyperblazexd@gmail.com>
2024-04-08 21:24:09 +0300

    success icon"

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,12 @@
+const colors = require('tailwindcss/colors');
 /** @type {import('tailwindcss').Config} */
 module.exports = {
 	content: ['./src/**/*.{html,js}'],
 	theme: {
 		extend: {
+			colors: {
+				dark: colors.gray[900],
+			},
 			fontFamily: {
 				crima: ['Crima'],
 			},
