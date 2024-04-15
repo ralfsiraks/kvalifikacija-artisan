12bda793a4defca769fff6ef00e725dc5d837e95
ralfsiraks <hyperblazexd@gmail.com>
2024-04-16 00:50:20 +0300

    spline sphere

--- src/app/components/main-banner/main-banner.component.ts
+++ src/app/components/main-banner/main-banner.component.ts
@@ -1,4 +1,4 @@
-import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
+import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
 
 @Component({
@@ -8,15 +8,4 @@ import { RouterLink } from '@angular/router';
 	templateUrl: './main-banner.component.html',
 	styleUrl: './main-banner.component.scss',
 })
-export class MainBannerComponent implements AfterViewInit {
-	@ViewChild('myIframe') myIframe: ElementRef;
-
-	ngAfterViewInit(): void {
-		const iframe = this.myIframe.nativeElement;
-		iframe.onload = () => {
-			const iframeDocument = iframe.contentDocument || iframe.contentWindow.document;
-			const elementWithinIframe = iframeDocument.querySelector('.spline-watermark');
-			console.log(elementWithinIframe); // This will log the element you want
-		};
-	}
-}
+export class MainBannerComponent {}
